db00b033252801691cd01faca18da916eb7cfa30
Administrator <253479240@qq.com>
2017-10-07 14:44:56 +0800

    first version

---
+++ src/main/java/com/zskang/test/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.zskang.test;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime=df.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
