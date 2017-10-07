a7658d5a4294da925fd5053c6a46aa6f624d47a1
Administrator <253479240@qq.com>
2017-10-07 23:02:48 +0800

    first version

--- src/main/java/com/zskang/utils/DatabaseHelper.java
+++ src/main/java/com/zskang/utils/DatabaseHelper.java
@@ -1,7 +1,10 @@
 package com.zskang.utils;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,26 +12,26 @@ import java.util.List;
 import java.util.Properties;
 
 public class DatabaseHelper {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseHelper.class);
     private static final QueryRunner QUERY_RUNNER = new QueryRunner();
     private static String URL = "";
     private static String USERNAME = "";
     private static String PASSWORD = "";
     private static final ThreadLocal<Connection> CONNECTION_THREAD_LOCAL_HOLDER = new ThreadLocal<Connection>();
 
-    static {
-        Properties properties = PropUtil.loadProps("db.propertise");
-        URL = PropUtil.getString(properties, "jdbc.url");
-        USERNAME = PropUtil.getString(properties, "jdbc.username");
-        PASSWORD = PropUtil.getString(properties, "jdbc.password");
-    }
-
     public static Connection getConnection() {
         Connection conn = CONNECTION_THREAD_LOCAL_HOLDER.get();
         if (conn == null) {
             try {
+                Properties properties = PropUtil.loadProps("db.propertise");
+                URL = PropUtil.getString(properties, "jdbc.url");
+                USERNAME = PropUtil.getString(properties, "jdbc.username");
+                PASSWORD = PropUtil.getString(properties, "jdbc.password");
                 conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             } catch (Exception e) {
                 e.printStackTrace();
+            }finally {
+                CONNECTION_THREAD_LOCAL_HOLDER.set(conn);
             }
         }
         return conn;
@@ -45,6 +48,20 @@ public class DatabaseHelper {
             closeConnection();
         }
         return entityList;
+    }
+
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity = null;
+        try {
+            Connection conn = DatabaseHelper.getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (Exception e) {
+            logger.error("queryEntity failure ", e);
+            e.printStackTrace();
+        }finally {
+            closeConnection();
+        }
+        return entity;
     }
 
     private static void closeConnection() {
