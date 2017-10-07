67218ab2fd06f3c7ebfdddef1dadfe334aa8f937
Administrator <253479240@qq.com>
2017-10-07 15:36:45 +0800

    first version

---
+++ src/main/java/com/zskang/bus/customer/model/CustomerVO.java
@@ -0,0 +1,66 @@
+package com.zskang.bus.customer.model;
+
+import java.io.Serializable;
+
+public class CustomerVO implements Serializable {
+
+    private int id;
+
+    private String name;
+
+    private String contact;
+
+    private String telphone;
+
+    private String remark;
+
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/zskang/bus/customer/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.zskang.bus.customer.service;
+
+import com.zskang.bus.customer.model.CustomerVO;
+
+import java.util.List;
+
+public class CustomerService {
+
+    public List<CustomerVO> getCustomerList(String keyword) {
+        return null;
+    }
+
+    public CustomerVO getDetailInfo(int id) {
+
+        return null;
+    }
+
+}
+
