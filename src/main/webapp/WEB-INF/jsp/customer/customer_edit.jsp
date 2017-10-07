8c47b2f6daa3b7ce687ee5e1cf101851771681ad
Administrator <253479240@qq.com>
2017-10-07 16:56:27 +0800

    first version

---
+++ src/main/webapp/WEB-INF/jsp/customer/customer_edit.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2017/10/7/007
+  Time: 16:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>编辑客户信息</title>
+</head>
+<body>
+
+</body>
+</html>
--- src/test/java/com/zskang/bus/customer/test/CustomerServiceTest.java
+++ src/test/java/com/zskang/bus/customer/test/CustomerServiceTest.java
@@ -6,7 +6,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomerServiceTest {
     private final CustomerService customerService;
@@ -21,11 +23,47 @@ public class CustomerServiceTest {
     }
 
     @Test
-    public void getCustomerListTest() throws Exception{
-        List<CustomerVO> customerVOList=this.customerService.getCustomerList("");
-        Assert.assertEquals(2,customerVOList.size());
+    public void getCustomerListTest() throws Exception {
+        List<CustomerVO> customerVOList = this.customerService.getCustomerList("");
+        Assert.assertEquals(2, customerVOList.size());
     }
 
+    @Test
+    public void getCustomerTest() throws Exception {
+        int id = 1;
+        CustomerVO customerVO = this.customerService.getDetailInfo(id);
+        Assert.assertNotNull(customerVO);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "test");
+        map.put("contact", "aaaaa");
+        map.put("telphone", "13244444444");
+        map.put("email", "1111111@ww.com");
+        boolean b = this.customerService.createCustomer(map);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        int id = 1;
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "00000");
+        map.put("contact", "01111111111111111111111");
+        map.put("telphone", "22222222222");
+        map.put("email", "eeeeeee@44.com");
+        boolean b = this.customerService.updateCustomer(id, map);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        int id = 1;
+        boolean b = this.customerService.deleteCustomer(id);
+        Assert.assertTrue(b);
+    }
 
 
 }
