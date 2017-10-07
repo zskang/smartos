4fabe65cd6f92d1cb47525a8d0ff60d46c2a6c70
Administrator <253479240@qq.com>
2017-10-07 15:29:05 +0800

    first version

---
+++ src/main/java/com/zskang/bus/customer/controller/CustomerCreateServlet.java
@@ -0,0 +1,8 @@
+package com.zskang.bus.customer.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends HttpServlet {
+}
---
+++ src/main/java/com/zskang/bus/customer/controller/CustomerDeleteServlet.java
@@ -0,0 +1,8 @@
+package com.zskang.bus.customer.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/customer-delete")
+public class CustomerDeleteServlet extends HttpServlet {
+}
